7a64b293b348bb83794b02fa7cce003dad2bab65
Tong Ding <dingtong@seattleu.edu>
2020-04-22 02:44:19 -0700

    working

--- warmup_cli.cpp
+++ warmup_cli.cpp
@@ -1,28 +1,19 @@
 /**
  * A simple client in IPv4 internet domain using TCP to 
  * send messages to a server and receive the responses. 
- * The hostname and port numbers are passed as arguments.
+ * The hostname and port number are passed as arguments.
  * @author Tong (Debby) Ding
  * @version 1.0
  * @see CPSC 5510 Spring 2020, Seattle University
  */
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <arpa/inet.h>
-// #include <strings.h>
-// #include <limits.h>
-// #include <sys/types.h>
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-
 #include <iostream>
-#include <unistd.h> // close()
+#include <unistd.h> // close
 #include <netdb.h>
-#include <cstring> // strlen(), bcopy(), ...
+#include <cstring>  // strlen, etc.
 
-#define BUF_SIZE 131072
-#define EXIT_WORD "quit"
+#define BUF_SIZE 131073 
+#define EXIT_WORD "quit"  
 #define PORT_MAX 12000
 #define PORT_MIN 11000
 
@@ -36,109 +27,76 @@ void error(const char *msg) {
     perror(msg);
     exit(1);
 }
-/**
- * Print a note message and exit the program
- * @param msg customized note message
- */ 
-void note(const char *msg) {
-    printf("Note: %s\n", msg);
-    exit(1);
-}
 
 /**
- * Convert user input port number from string to int,
- * or print the error and exit if validation fails
- * @param str the string of port number to convert
- * @return the int of port number
- */
-int convertPort(const string& str) {
-	char *end;
-    long val = strtol(str.c_str(), &end, 10);
-
-	// no digits or incomplete conversion
-    if (end == str || *end != '\0')
-		note("port number must all be digits");
-
-	// number out of range
-	if (val < PORT_MIN || val > PORT_MAX)
-		note("port number out of range");
-
-	return (int) val;
-}
-
-/**
- * Send a message to server and print the response
+ * Send messages to server through TCP socket and print the response
  * @param argc number of arguments entered by user
  * @param argv[] the content of arguments
  */ 
 int main(int argc, char *argv[]) {
 
-	int sock, port, err;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+	int sock, err;                    // socket and error number
+	struct addrinfo hints, *ser_addr; // server address
+    char buffer[BUF_SIZE];            // message buffer size
 
-    char buffer[BUF_SIZE];
-
-	// check user input hostname & port number
+	// check if user entered hostname and port number
     if (argc != 3)
-		note("Usage example \"./warmup_cli HOSTNAME HOSTPORT\"");
-
-	// get the port number with validation
-	port = convertPort(argv[2]);
+		error("Error usage i.e. \"./warmup_cli HOSTNAME HOSTPORT\"");
 
 	// create a socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock < 0) 
-        error("ERROR: fail to open socket");
+        error("ERROR create socket");
 
-
-
-	// FIX-ME
-    server = gethostbyname(argv[1]);
-	if (server == NULL) 
-		error("ERROR: host not found");
+	// specify criteria for selecting socket address
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET;        // IPv4
+	hints.ai_socktype = SOCK_STREAM;  // TCP
+	hints.ai_flags = 0;
+	hints.ai_protocol = 0;       
 	
-	// setup addr for server
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(port);
+	// get server address information
+	err = getaddrinfo(argv[1], argv[2], &hints, &ser_addr);
+	if (err < 0)
+		error("ERROR host not found");
 
+	// connect to the server
+	err = connect(sock, ser_addr->ai_addr, ser_addr->ai_addrlen); 
+	if (err < 0) 
+		error("ERROR connect server");	
 
-	if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-		error("ERROR connecting");	
-
+	// instruction to users
 	printf("\nWelcome! Enter a message to send or \"quit\" to exit.");
 	
-	// user-input loop
+	// start user-input loop
 	while (true) {
 
+		// get user input
 		printf("\n\nSend: ");
+		bzero(buffer, BUF_SIZE);
+		if (fgets(buffer, BUF_SIZE, stdin) == NULL)
+			error("ERROR input message");
 
-		bzero(buffer,BUF_SIZE);
-		fgets(buffer,BUF_SIZE,stdin);
-
-		// send message and check for error
+		// send message to server
 		err = send(sock, buffer, strlen(buffer), 0);
 		if (err < 0) 
-			error("ERROR writing to socket");
-
-		// clear buffer
-		bzero(buffer,BUF_SIZE);
+			error("ERROR send message");
 
 		// receive message
+		bzero(buffer,BUF_SIZE);
 		err = recv(sock, buffer, BUF_SIZE, 0);
 		if (err < 0) 
-			error("ERROR reading from socket");
+			error("ERROR receive message");
 
-		// exit if user enter "quit"
+		// exit if input is "quit"
 		if (strncmp(buffer, EXIT_WORD, strlen(EXIT_WORD)) == 0) 
 			break;
 		
+		// print out received message
 		printf(buffer);
-		
 	}
 
+	// close socket when done
     close(sock);
  
     return EXIT_SUCCESS;
--- warmup_svr.cpp
+++ warmup_svr.cpp
@@ -1,26 +1,19 @@
 /**
  * A simple server in IPv4 internet domain using TCP to receive 
- * messages from clients and reply with the messages converted. 
+ * messages from clients and reply with the messages inverted. 
  * The port number is passed as an argument.
  * @author Tong (Debby) Ding
  * @version 1.0
  * @see CPSC 5510 Spring 2020, Seattle University
  */
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <strings.h>
-// #include <sys/types.h> 
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-
 #include <iostream>
-#include <unistd.h> // close()
-#include <arpa/inet.h> // inet_ntoa()
-#include <cstring> // strlen(), bzero(), ...
-#include <algorithm> // reverse()
+#include <unistd.h>    // close
+#include <arpa/inet.h> // inet_ntoa
+#include <cstring>     // strlen, stc.
+#include <algorithm>   // reverse
 
-#define BUF_SIZE 131072
+#define BUF_SIZE 131073
 #define EXIT_WORD "quit"
 #define PORT_MAX 12000
 #define PORT_MIN 11000
@@ -36,14 +29,6 @@ void error(const char *msg) {
     perror(msg);
     exit(1);
 }
-/**
- * Print a note message and exit the program
- * @param msg customized note message
- */ 
-void note(const char *msg) {
-    printf("Note: %s\n", msg);
-    exit(1);
-}
 
 /**
  * Convert user input port number from string to int,
@@ -57,11 +42,11 @@ int convertPort(const string& str) {
 
 	// no digits or incomplete conversion
     if (end == str || *end != '\0')
-		note("port number must all be digits");
+		error("Error port number (not all digits)");
 		
 	// number out of range
 	if (val < PORT_MIN || val > PORT_MAX)
-		note("port number out of range");
+		error("Error port number (out of range)");
 
 	return (int) val;
 }
@@ -73,85 +58,94 @@ int convertPort(const string& str) {
  */ 
 int main(int argc, char *argv[]) {
 
-	int sockfd, newsockfd, port, err;
-	socklen_t clilen;
-	char buffer[BUF_SIZE];
-	struct sockaddr_in serv_addr, cli_addr;
-	int n;
-	
-	// check user input port number
+	int sock, tmp_sock;                     // sockets for listening and message transfer
+	int port, err;                          // port and error number
+	char buffer[BUF_SIZE];                  // message buffer size
+	struct sockaddr_in ser_addr, cli_addr;  // server and client addresses
+	socklen_t cli_size;                     // client address size
+
+	// check if user entered port number
     if (argc != 2)
-		note("Usage example \"./warmup_cli HOSTPORT\"");
+		error("Error usage i.e. \"./warmup_svr HOSTPORT\"");
 
 	// get the port number with validation
 	port = convertPort(argv[1]);
 
 	// create a socket
-	sockfd =  socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) 
-		error("ERROR opening socket");
+	sock =  socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) 
+		error("ERROR create socket");
 
-	// clear address structure
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+	// get server address information
+	bzero((char *) &ser_addr, sizeof(ser_addr));	
+	ser_addr.sin_family = AF_INET;  
+	ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);  
+	ser_addr.sin_port = htons(port);
 
+	// bind socket to server address
+	err = bind(sock, (struct sockaddr *) &ser_addr, sizeof(ser_addr));
+	if (err < 0) 
+		error("ERROR bind socket");
 
-
-	// setup the addr for server to bind the socket
-	serv_addr.sin_family = AF_INET;  
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);  
-	serv_addr.sin_port = htons(port);
-
-	// bind the socket to the addr of server
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-			error("ERROR on binding");
-
-
-	// start the loop to get connected from clients
+	// start listening loop
 	while (true) {
 
-		// let socket listen to incoming connection requests
-		err = listen(sockfd, LISTEN_BKLOG);
+		// listen to incoming connection requests
+		err = listen(sock, LISTEN_BKLOG);
 		if (err < 0) 
-			error("ERROR on listen");
+			error("ERROR socket listen");
 
-		clilen = sizeof(cli_addr); // size of client addr info structure
+		cli_size = sizeof(cli_addr);
 
-		// accepts a connection request and open a new socket to free the original socket
-		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-		if (newsockfd < 0) 
-			error("ERROR on accept");
+		// accept connection and open new socket for data transfer
+		tmp_sock = accept(sock, (struct sockaddr *) &cli_addr, &cli_size);
+		if (tmp_sock < 0) 
+			error("ERROR accept client");
 
 		printf("Accepted connection from %s port %d\n\n",
 			inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));	
 
+		// start message exchange loop
 		while (true) {
+
+			// receive message
 			bzero(buffer, BUF_SIZE);
+			err = recv(tmp_sock, buffer, BUF_SIZE, 0);
+			if (err < 0)
+				error("ERROR receive message");
 
-			n = recv(newsockfd,buffer, BUF_SIZE, 0);
-			if (n < 0)
-				error("ERROR reading from socket");
-
-			// prepare to disconnect if user send "quit"
+			// prepare to close socket if get "quit" from client
 			if (strncmp(buffer, EXIT_WORD, strlen(EXIT_WORD)) == 0) {
 				printf("Connection terminated by %s port %d\n\n",
 					inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
 
-				send(newsockfd, buffer, strlen(buffer), 0);
+				send(tmp_sock, buffer, strlen(buffer), 0);
 				break;
 			}
 
 			printf("Received message: %s\n", buffer);
 
-			// reverse received message
-			reverse(buffer, buffer + strlen(buffer));
+			try {
+				// reverse received message
+				reverse(buffer, buffer + strlen(buffer));
 
-			// send the revised message
-			send(newsockfd, buffer, strlen(buffer), 0);
+				// send revised message
+				err = send(tmp_sock, buffer, strlen(buffer), 0);
+				if (err < 0) 
+					error("ERROR send message");
 
+			} catch (const char* msg) {
+				// send error message
+				err = send(tmp_sock, msg, strlen(msg), 0);
+				if (err < 0) 
+					error("ERROR send message");
+			}
 		}
-		close(newsockfd);
-	}	
-	close(sockfd); 
+		// close socket when client disconnected
+		close(tmp_sock);
+	}
+	// close socket when listening stopped	
+	close(sock); 
 	
     return EXIT_SUCCESS;
 }
